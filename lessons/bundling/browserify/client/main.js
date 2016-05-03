de56a3e7380728df25e2e2505728718335684f56
Angad Singh <Angad Singh>
2016-05-03 09:23:34 -0400

    Added a comment

--- lessons/bundling/browserify/client/main.js
+++ lessons/bundling/browserify/client/main.js
@@ -1,4 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+//Hey this is react
 var App = require('./components/app.js');
 ReactDOM.render(<App/>, document.getElementById('content'));
